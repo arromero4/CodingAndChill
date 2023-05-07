8654389bc5e64f556adb37481fb36b091db81fc2
arromero4 <arromero0408@gmail.com>
2023-05-06 21:02:43 -0600

    Practicing my coding skills

---
+++ react-midu-zero/learning-react/src/App.jsx
@@ -0,0 +1,16 @@
+export function App() {
+  return (
+    <article>
+      <header>
+        <img src="https://unavatar.io/arromero491" alt="Mi avatar" />
+        <div>
+          <strong>Andrés Romero</strong>
+          <span>@arromero491</span>
+        </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
