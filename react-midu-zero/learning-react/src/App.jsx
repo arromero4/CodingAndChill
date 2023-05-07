bd6eb083466cb4f8a4a5cf293970ce2d12c921bd
arromero4 <arromero0408@gmail.com>
2023-05-07 00:01:13 -0600

    Practicing my coding skills

--- react-midu-zero/learning-react/src/App.jsx
+++ react-midu-zero/learning-react/src/App.jsx
@@ -1,15 +1,45 @@
 import { TwitterFollowCard } from "./TwitterFollowCard";
+
+const users = [
+  {
+    id:1,
+    userName:'arromero491',
+    name: 'Andres Romero',
+    isFollowing: true
+  },
+  {
+    id:2,
+    userName:'midudev',
+    name: 'Miguel Angel Duran',
+    isFollowing: false
+  },
+  {
+    id:3,
+    userName:'yoshimi_robot',
+    name: 'Yoshimi O Neil',
+    isFollowing: true
+  }
+]
+
 export function App() {
   
   return (
     <section className="App">
-      <TwitterFollowCard isFollowing username="arromero491">
-        Andres Romero
-      </TwitterFollowCard>
-
-      <TwitterFollowCard isFollowing={false} username="midudev">
-        Miguel Angel Duran
-      </TwitterFollowCard>
+      {
+        users.map(user => {
+          const {id, userName, name, isFollowing} = user
+          console.log(userName)
+          return(
+            <TwitterFollowCard
+              key={id}
+              userName={userName}
+              initialIsFollowing={isFollowing}
+            >
+              {name}
+            </TwitterFollowCard>
+          )
+        })
+      }
     </section>
   );
 }
--- react-midu-zero/learning-react/src/TwitterFollowCard.jsx
+++ react-midu-zero/learning-react/src/TwitterFollowCard.jsx
@@ -1,23 +1,33 @@
-export function TwitterFollowCard({
-  children,
-  username='unknown',
-  isFollowing,
-}) {
+import { useState } from "react";
+export function TwitterFollowCard({ children, userName, initialIsFollowing }) {
+  console.log(userName)
+  const [isFollowing, setIsFollowing ] = useState(initialIsFollowing);
+
+  const text = isFollowing ? 'Following' : 'Follow'
+  const buttonClassName = isFollowing ? 'tw-followCard-button is-following':'tw-followCard-button'
+
+  const handleClick = () => {
+    setIsFollowing(!isFollowing)
+  }
+
   return (
     <article className="tw-followCard">
       <header className="tw-followCard-header">
         <img
           className="tw-followCard-avatar"
-          src={`https://unavatar.io/${username}`}
+          src={`https://unavatar.io/${userName}`}
           alt="Avatar username"
         />
         <div className="tw-followCard-info">
           <strong>{children}</strong>
-          <span className="tw-followCard-info-userName">@{username}</span>
+          <span className="tw-followCard-info-userName">@{userName}</span>
         </div>
       </header>
       <aside>
-        <button className="tw-followCard-button">Seguir</button>
+        <button className={buttonClassName} onClick={handleClick}>
+        <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Unfollow</span>
+        </button>
       </aside>
     </article>
   );
