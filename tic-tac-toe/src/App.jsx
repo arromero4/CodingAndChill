3463dc59ce7b2835747a3020be3ded5a06c1ebd4
arromero4 <arromero0408@gmail.com>
2023-05-07 22:27:55 -0600

    Practicing my coding skills

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "./App.css";
+
+const TURNS = {
+  X: "x",
+  O: "o",
+};
+
+const Square = ({ children, updateBoard, index }) => {
+  return <div className="square">{children}</div>;
+};
+function App() {
+
+  const board = Array(9).fill(null);
+  
+  
+  
+  return (
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
+      <section className="game">
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              
+            </Square>
+          );
+        })}
+      </section>
+    </main>
+  );
+}
+
+export default App;
