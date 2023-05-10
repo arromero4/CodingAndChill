0cd652f56634b9e4e6c7b79e62d8479db5c5c7b7
arromero4 <arromero0408@gmail.com>
2023-05-09 23:28:30 -0600

    Practicing my coding skills

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -6,26 +6,92 @@ const TURNS = {
   O: "o",
 };
 
-const Square = ({ children, updateBoard, index }) => {
-  return <div className="square">{children}</div>;
+const WINNER_COMBOS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6],
+]
+
+
+const Square = ({ children, updateBoard,isSelected, index }) => {
+
+  const className = `square ${isSelected ? 'is-selected': ''}`
+  
+  const handleClick = () => {
+    updateBoard(index)
+  }
+  
+  return(
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div> 
+  )
+    
 };
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null)//null es que no hay ganador, false es que hay un empate
 
-  const board = Array(9).fill(null);
-  
-  
-  
+  const checkWinner = (boardToCheck) => {
+    //Revisamos si todas las combinaciones ganadoras 
+    //para ver si ganó X u O
+    for(const combo of WINNER_COMBOS){
+      const [a,b,c] = combo
+      if(
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+        return boardToCheck[a]
+      }
+    }
+    //si no hay ganador
+    return null
+  }
+
+
+  const updateBoard = (index) =>{
+
+    //no se actualiza si ya tiene algo
+    if(board[index] || winner) return
+    //Actualiza tablero
+    const newBoard = [...board]
+    newBoard[index] = turn 
+    setBoard(newBoard)
+    //cambiar el turno
+    const newTurn = turn === TURNS.X ? TURNS.O: TURNS.X
+    setTurn(newTurn)
+    //Revisar si hay un ganador
+    const newWinner = checkWinner(newBoard)
+    if(newWinner){
+      setWinner(newWinner)
+    }
+  }
+
+
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section className="game">
         {board.map((_, index) => {
-          return (
-            <Square key={index} index={index}>
-              
-            </Square>
-          );
+          return <Square 
+          key={index} 
+          index={index}
+          updateBoard={updateBoard}
+          >
+            {board[index]}
+          </Square>;
         })}
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
     </main>
   );
