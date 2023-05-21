2fac722b5e081183ebda6d95632964213d880fca
arromero4 <arromero0408@gmail.com>
2023-05-21 01:01:55 -0600

    Practicing coding skills - learning react memo, useMemo, useCallback

---
+++ hooks-memo-useMemo-useCallback-2/src/Encabezado.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Encabezado = () => {
+  console.log("Encabezado renderizado");
+  return (
+    <div className="encabezado">
+      <h1>Contadores</h1>
+    </div>
+  );
+};
+
+export default Encabezado;
