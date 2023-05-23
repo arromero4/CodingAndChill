40d5cb6e81686c79caf93342f503fae389db29e3
arromero4 <arromero0408@gmail.com>
2023-05-23 01:27:47 -0600

    Practicing coding skills

--- tiny-projects-python/binarySearch.py
+++ tiny-projects-python/binarySearch.py
@@ -1,8 +1,36 @@
 """
 a function that takes a list and target parameter
 multiple variables: middle, start, end, steps
-
-
+recursion or while loop
+increase the steps each the split is done
+conditions to track target position
 [1,2,3,4,5,6,7,8,9,10]
 
 """
+def binary_search(list, target):
+    middle = 0
+    start = 0
+    end = len(list)
+    steps = 0
+
+    while start <= end:
+        print("Step", steps, ":", str(list[start:end+1]), "\n",middle, "\n")
+
+        steps += 1
+        middle = (start + end) // 2
+
+
+        if target == list[middle]: 
+            return middle
+        
+        if target < list[middle]:
+            end = middle - 1
+            print("se actualiza end: ", end)
+        else:
+            start = middle + 1
+            print("se actualiza start: ",start)
+    return -1
+
+my_list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+target = 16
+binary_search(my_list, target)
