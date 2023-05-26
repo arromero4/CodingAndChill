78a756ec3d7fa8eb444c6a9d2e40583454e51edf
arromero4 <arromero0408@gmail.com>
2023-05-26 03:22:48 -0600

    Practicing coding skills

---
+++ nextjs-learning/pages/index.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+      function Header({ title }){
+        return (
+          <h1>{title ? title : 'Default Title'}</h1>
+          )
+      }
+
+      export default function HomePage(){
+        const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+        const [likes, setLikes] = useState(0)
+
+        
+
+        function handleClick(){
+          console.log("Increment like count")
+          setLikes(likes + 1)
+        }
+
+        return(
+          <div>
+            <Header title="React 💙" />
+            <Header title="Develop. Preview. Ship. 🚀" />
+            <ul>
+              {
+                names?.map(name => (
+                    <li key={name}>{name}</li>
+                  ))
+              }
+            </ul>
+            <button onClick={handleClick}>Like ({likes})</button>
+          </div>
+        )
+      }
+
