9103f9b4147a89a27a60e127956fac60ef1f360b
arromero4 <arromero0408@gmail.com>
2023-06-07 01:20:56 -0600

    Practicing coding skills

---
+++ ciscoSkills/test.py
@@ -0,0 +1,16 @@
+a = 6
+b = 3
+a /= 2 *b
+#a = a / 2 * b
+print(a)
+ 
+
+"""
+ 
+Prioridad	Operador	
+1	**	
+2	+, - (nota: los operadores unarios a la derecha del operador exponencial enlazan con mayor fuerza.)	unario
+3	*, /, //, %	
+4	+, -	binario
+
+"""
