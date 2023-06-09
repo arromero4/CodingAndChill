a4e46a5e317add0941f968f1c1507ea3612c98db
arromero4 <arromero0408@gmail.com>
2023-06-09 01:18:21 -0600

    Practicing coding skills

--- ciscoSkills/largerNumber.py
+++ ciscoSkills/largerNumber.py
@@ -20,3 +20,42 @@ else:
     larger_number = number2 
 # Print the result \
 print("The larger number is:", larger_number)
+
+
+#Example with break 
+largest_number = -99999999
+counter = 0
+
+while True:
+    number = int(input("Enter a number or type -1 to end the program: "))
+    if number == -1:
+        break
+    counter += 1
+    if number > largest_number:
+        largest_number = number
+
+if counter != 0:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
+
+# Example with  continue
+largest_number = -99999999
+counter = 0
+
+number = int(input("Enter a number or type -1 to end program: "))
+
+while number != -1:
+    if number == -1:
+        continue
+    counter += 1
+
+    if number > largest_number:
+        largest_number = number
+    number = int(input("Enter a number or type -1 to end the program: "))
+
+if counter:
+    print("The largest number is", largest_number)
+else:
+    print("You haven't entered any number.")
+
---
+++ ciscoSkills/powerOfTwo.py
@@ -0,0 +1,8 @@
+power = 1 #1 2 4 8
+for expo in range(16): #0 1 2 3
+    print("2 to the power of", expo, "is", power)
+    power *= 2 #2 4 8 16
+#2 to the power of 0 is 1
+#2 to the power of 1 is 2
+#2 to the power of 2 is 4
+#2 to the power of 3 is 8
