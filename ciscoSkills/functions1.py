6619e023548bc002130315164a7d4e24c2bd4dbd
arromero4 <arromero0408@gmail.com>
2023-06-13 00:50:15 -0600

    Practicing coding skills

---
+++ ciscoSkills/functions1.py
@@ -0,0 +1,18 @@
+# def introduction(first_name, last_name):
+#     print("Hello, my name is", first_name, last_name)
+ 
+# introduction("Luke", "Skywalker")
+# introduction("Jesse", "Quick")
+# introduction("Clark", "Kent")
+
+# def introduction(first_name, last_name):
+#     print("Hello, my name is", first_name, last_name)
+ 
+# introduction(first_name = "James", last_name = "Bond")
+# introduction(last_name = "Skywalker", first_name = "Luke")
+
+def introduction(first_name, last_name="Smith"):
+     print("Hello, my name is", first_name, last_name)
+introduction("James", "Doe")
+introduction("Henry")
+introduction(first_name="William")
