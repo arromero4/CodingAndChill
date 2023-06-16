21d968200e4e2bde94c8d88204fefcde1bea2be3
arromero4 <arromero0408@gmail.com>
2023-06-16 02:00:12 -0600

    Practicing coding skills

--- ciscoSkills/dictionary3.py
+++ ciscoSkills/dictionary3.py
@@ -6,3 +6,16 @@ dictionary['cat'] = 'minou'
 #Adding value and key to dictionary
 dictionary['swan'] = 'cygne'
 print(dictionary)
+
+
+#Adding an item to dictionary
+dictionary.update({"duck": "canard"})
+print(dictionary)
+
+#Removing an item from dictionary
+del dictionary['dog']
+print(dictionary)
+
+#Removing the last item from dictionary
+dictionary.popitem()
+print(dictionary)
