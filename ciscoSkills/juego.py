d9d3d132eef6cc62facdd251fcfdd359d4c8e47e
arromero4 <arromero0408@gmail.com>
2023-06-19 22:11:22 -0600

    Practicing coding skills

---
+++ ciscoSkills/juego.py
@@ -0,0 +1,15 @@
+tablero = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+]
+
+def mostrar_tablero():
+    print("")
+    for fila in tablero:
+        print(fila)
+
+def juego():
+    mostrar_tablero()
+
+juego()
--- ciscoSkills/tictactoe.py
+++ ciscoSkills/tictactoe.py
@@ -147,25 +147,33 @@ for i in range(10):
 
 """
 
-def display_board(board):
+def display_board(board): 
+    #display_board(board)
     # The function accepts one parameter containing the board's current status
     # and prints it out to the console.
+    print("+-------" * 3,"+", sep="")
+    for row in range(3):
+        print("|       " * 3,"|", sep="")
+        for col in range(3):
+            print("|   " + str(board[row][col]) + "   ", end="")
+        print("|")
+        print("|       " * 3,"|",sep="")
+        print("+-------" * 3,"+",sep="")
 
-
-def enter_move(board):
+#def enter_move(board):
     # The function accepts the board's current status, asks the user about their move, 
     # checks the input, and updates the board according to the user's decision.
 
 
-def make_list_of_free_fields(board):
+#def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares; 
     # the list consists of tuples, while each tuple is a pair of row and column numbers.
 
 
-def victory_for(board, sign):
+#def victory_for(board, sign):
     # The function analyzes the board's status in order to check if 
     # the player using 'O's or 'X's has won the game
 
 
-def draw_move(board):
+#def draw_move(board):
     # The function draws the computer's move and updates the board.
