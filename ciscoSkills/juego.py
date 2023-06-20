d2ef300e85ee154c2dae3872db0a0cabbb2e61ac
arromero4 <arromero0408@gmail.com>
2023-06-20 02:13:04 -0600

    Practicing coding skills

--- ciscoSkills/juego.py
+++ ciscoSkills/juego.py
@@ -1,15 +1,41 @@
-tablero = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "],
-]
+board = [
+    "-", "-", "-",
+    "-", "-", "-",
+    "-", "-", "-",
+         ]
+currentPlayer = "X"
+winner = None
+gameRunning = True
 
-def mostrar_tablero():
-    print("")
-    for fila in tablero:
-        print(fila)
+#printing the game board
+def printBoard(board):
+    print("----------------")
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("----------------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("----------------")
+    print(board[6] + " | " + board[7] + " | " + board[8])
+    print("----------------")
 
-def juego():
-    mostrar_tablero()
+#take player input
+def playerInput(board):
+    inp = int(input("Enter a number 1-9: "))
+    if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("Oops player is already in that spot")
 
-juego()
+
+#check for win or tie
+def checkHorizontal(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        winner = board[0]
+        return True
+
+#switch the player
+#check for win or tie again
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
+
