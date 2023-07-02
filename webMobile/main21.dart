92881926222b25b24e5c1f71d63714b870b05f92
arromero4 <arromero0408@gmail.com>
2023-07-02 02:31:00 -0600

    Adding DART from codigofacilito

--- webMobile/main21.dart
+++ webMobile/main21.dart
@@ -1,19 +1,36 @@
 void main() {
-  print('Jose');
-  bienvenida();
-  print('Maria');
-  bienvenida();
+  bienvenida('Jose', 'Nuevo');
+  bienvenida('Maria', 'Antiguo');
+  bienvenida('Gian', 'Top', posicion: 1);
 }
 
-bienvenida() {
-  int descuento = 50;
+bienvenida(String nombre, String tipo, {int posicion = 10}) {
+  int descuento = 0;
+  String mensaje = '';
+  switch (tipo) {
+    case 'Nuevo':
+      descuento = 50;
+      mensaje = 'Por ser la primera vez aquí';
+      break;
+    case 'Antiguo':
+      descuento = 60;
+      mensaje = 'Por ser un usuario antiguo';
+
+      break;
+    case 'Top':
+      descuento = 70;
+      mensaje = 'Por ser un alumno top y tener el lugar $posicion';
+      break;
+  }
+
+  descuentoUsuario(descuento);
+  print(
+      '$nombre bienvenido a codigofacilito, en este curso aprenderas DART. $mensaje, se aplicará descuento del $descuento%');
+}
+
+descuentoUsuario(int descuento) {
   int precio = 1456;
-  print('''Bienvenido a codigofacilito, 
-  en este curso aprenderas DART. 
-  Por ser la primera vez, 
-  se aplicará descuento.''');
   double aplicarDescuento = (precio * descuento) / 100;
   double precioTotal = precio - aplicarDescuento;
-
   print('Precio con descuento: $precioTotal');
 }
