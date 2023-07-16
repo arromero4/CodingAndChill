3d331d6c06028499fadfadcd3e70ff6f62be6b28
arromero4 <arromero0408@gmail.com>
2023-07-16 17:41:27 -0600

    Saludando a la comunidad

---
+++ dart-moure/dart_1.dart
@@ -0,0 +1,29 @@
+void main() {
+  //variables
+
+  //<datatype> <variablename> = value;
+  //double firstValue = 19.5;
+  //double secondValue = 62.22;
+  //String firstValue = 'hello'
+  // firstValue = true;
+  //dynamic firstValue = 10;
+  //String greeting = 'hello world';
+  //print('My first ${greeting.length}');
+
+  //var/final/const
+
+  //var someValue = 10;
+  //final someValue2 = DateTime.now();
+  //const someValue3 = '10';
+
+  //Optional Variables
+  //String/int/bool and null
+
+  String? someValue;
+
+  print(someValue);
+
+  someValue = 'hello';
+
+  print(someValue);
+}
