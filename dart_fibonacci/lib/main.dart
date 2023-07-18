daeefffeb495a534a5de548a9fcb8babbd876012
arromero4 <arromero0408@gmail.com>
2023-07-18 01:51:08 -0600

    adding memory usage and stopwatch

--- dart_fibonacci/lib/main.dart
+++ dart_fibonacci/lib/main.dart
@@ -1,32 +1,11 @@
 import 'package:system_resources/system_resources.dart';
 
-void main() async {
-  await SystemResources.init();
-  int i = 10;
-  print('----------------------------------------------------------------');
-  print('Recursivo:');
-  final stopwatch1 = Stopwatch()..start();
-  int fibonacciRecursive = fibRecursive(10);
-  print('Fibonacci de $i es: ${fibonacciRecursive.toString()}');
-  stopwatch1.stop();
-  print('Function Execution Time : ${stopwatch1.elapsed}');
-  print('Memory Usage     : ${(SystemResources.memUsage() * 100).toInt()}%');
-  print('----------------------------------------------------------------');
-  print('Iterativo:');
-  final stopwatch2 = Stopwatch()..start();
-  int fibonacciIterative = fibIterative(10);
-  print('Fibonacci de $i es: ${fibonacciIterative.toString()}');
-  stopwatch2.stop();
-  print('Function Execution Time : ${stopwatch2.elapsed}');
-  print('Memory Usage     : ${(SystemResources.memUsage() * 100).toInt()}%');
-  print('----------------------------------------------------------------');
-}
-
 //Recursivo
 int fibRecursive(int n) {
   return n < 2 ? n : (fibRecursive(n - 1) + fibRecursive(n - 2));
 }
 
+//Iterativo
 int fibIterative(int n) {
   if (n <= 0) return 0;
   if (n == 1) return 1;
@@ -42,4 +21,27 @@ int fibIterative(int n) {
   }
 
   return result;
+}
+
+void main() async {
+  await SystemResources.init();
+  int i = 10;
+
+  print('Recursivo:');
+  final stopwatch1 = Stopwatch()..start();
+  int fibonacciRecursive = fibRecursive(10);
+  stopwatch1.stop();
+
+  print('Fibonacci de $i es: ${fibonacciRecursive.toString()}');
+  print('Tiempo de ejecución: ${stopwatch1.elapsed}');
+  print('Uso de memoria: ${(SystemResources.memUsage() * 100).toInt()}%');
+
+  print('Iterativo:');
+  final stopwatch2 = Stopwatch()..start();
+  int fibonacciIterative = fibIterative(10);
+  stopwatch2.stop();
+
+  print('Fibonacci de $i es: ${fibonacciIterative.toString()}');
+  print('Tiempo de ejecución: ${stopwatch2.elapsed}');
+  print('Uso de memoria: ${(SystemResources.memUsage() * 100).toInt()}%');
 }
