25388843d91ff258ec5c7931b3840d88ae22fcb1
arromero4 <arromero0408@gmail.com>
2023-07-26 02:52:38 -0600

    adding new practice

--- dart_tutorial_1/lib/dart_15.dart
+++ dart_tutorial_1/lib/dart_15.dart
@@ -2,6 +2,6 @@ import 'dart:io';
 
 void main() {
   print("Ingrese un número con decimal:");
-  double number = double.parse(stdin.readLineSync()!);
+  double? number = double.parse(stdin.readLineSync()!);
   print("El número es: $number");
 }
