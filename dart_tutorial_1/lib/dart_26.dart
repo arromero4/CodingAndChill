1fb77ee02b9c1548da56d3ef3d17be16f8d3ba41
arromero4 <arromero0408@gmail.com>
2023-07-28 00:03:43 -0600

    adding new practice

--- dart_tutorial_1/lib/dart_26.dart
+++ dart_tutorial_1/lib/dart_26.dart
@@ -1,6 +1,8 @@
 void main() {
   String text = "Andres";
-
   print("Texto original: $text");
+  //Texto original: Andres
+
   print("Unidad de código del indice 0: ${text.codeUnitAt(0)}");
+  //Unidad de código del indice 0: 65
 }
---
+++ dart_tutorial_1/lib/dart_28.dart
@@ -0,0 +1,6 @@
+void main() {
+  String text = "hello world";
+  print(
+      "Primera letra en mayúscula: ${text[0].toUpperCase()}${text.substring(1)}");
+  //Primera letra en mayúscula: Hello world
+}
