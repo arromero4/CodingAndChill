57ef690a9b0e28e137e1ef44e55487307ab50eb1
arromero4 <arromero0408@gmail.com>
2023-08-04 00:43:52 -0600

    adding new practice

---
+++ dart_tutorial_1/lib/dart_35.dart
@@ -0,0 +1,5 @@
+void main() {
+  var age = 18;
+  var check = (age >= 18) ? 'Puedes votar' : 'NO puedes votar';
+  print(check); //Puedes votar
+}
---
+++ dart_tutorial_1/lib/dart_36.dart
@@ -0,0 +1,6 @@
+void main() {
+  for (int i = 1; i <= 10; i++) {
+    print(i);
+    //1 2 3 4 5 6 7 8 9 10
+  }
+}
