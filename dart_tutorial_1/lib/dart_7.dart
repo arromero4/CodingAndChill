480cfb25472873956969a30e95e1266874bd4294
arromero4 <arromero0408@gmail.com>
2023-08-08 03:15:51 -0600

    adding new practice

---
+++ dart_tutorial_1/lib/dart_7.dart
@@ -0,0 +1,9 @@
+// Escribimos la función fuera de la función principal "main"
+void printName() {
+  print("Me llamo Andres, vengo de una función.");
+}
+
+// Esta es nuestra función principal "main"
+void main() {
+  printName();
+}
---
+++ dart_tutorial_1/lib/dart_8.dart
@@ -0,0 +1,10 @@
+//Aqui los parametros son num1 y num2
+void sumarNumeros(int num1, int num2) {
+  int sum = num1 + num2;
+  print("La suma es: $sum");
+}
+
+void main() {
+  //Los argumentos son 10 y 20
+  sumarNumeros(10, 20);
+}
