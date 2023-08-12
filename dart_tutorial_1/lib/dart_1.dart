ff8df60a9035b91902d62ad9624f9db7f4b7ff67
arromero4 <arromero0408@gmail.com>
2023-08-12 02:30:39 -0600

    adding new practice

--- dart_tutorial_1/lib/dart_1.dart
+++ dart_tutorial_1/lib/dart_1.dart
@@ -7,7 +7,12 @@ List<String> names = ["Raj", "John", "Rocky"];
 //Lista con elementos de distinto tipo.
 var mixed = [10, "John", 18.8];
 
+// void main() {
+//   var list = List<int>.filled(5, 0);
+//   print(list); //[0, 0, 0, 0, 0]
+// }
+
 void main() {
-  var list = List<int>.filled(5, 0);
-  print(list);
+  var list1 = [210, 21, 22, 33, 44, 55];
+  print(list1); //[210, 21, 22, 33, 44, 55]
 }
---
+++ dart_tutorial_1/lib/dart_3.dart
@@ -0,0 +1,6 @@
+void main() {
+  var list = [210, 21, 22, 33, 44, 55];
+
+  print(list.indexOf(22)); //index 2
+  print(list.indexOf(33)); //index 3
+}
