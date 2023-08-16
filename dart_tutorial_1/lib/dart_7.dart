39b2b9bb2f6bed8756a5465fba8c4ad80c138c0f
arromero4 <arromero0408@gmail.com>
2023-08-16 05:10:51 -0600

    adding new practice

--- dart_tutorial_1/lib/dart_7.dart
+++ dart_tutorial_1/lib/dart_7.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 
 void main() {
   //Abrir archivo
-  File file = File('C:\\Users\\test.txt');
+  File file = File(
+      'C:\\Users\\ARROMERO\\Desktop\\CodingAndChill\\dart_tutorial_1\\lib\\test.txt');
   //Leer archivo
   String contents = file.readAsStringSync();
   //mostramos
