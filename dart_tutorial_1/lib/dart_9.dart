c6965dae2bba2b418853dd943bd3bd5d6ecadf64
arromero4 <arromero0408@gmail.com>
2023-08-20 22:34:33 -0600

    adding new practice

---
+++ dart_tutorial_1/lib/dart_9.dart
@@ -0,0 +1,17 @@
+class Laptop {
+  String? brand;
+  int? prize;
+
+  // Constructor
+  Laptop() {
+    print("This is a default constructor");
+  }
+}
+
+void main() {
+  //Objecto de la clase Laptop
+  Laptop laptop = Laptop();
+
+  //Se mostrará:
+  //This is a default constructor
+}
