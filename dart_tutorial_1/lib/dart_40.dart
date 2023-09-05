43c3b7bc8f794016d2b4a45753b3078800925806
arromero4 <arromero0408@gmail.com>
2023-09-04 19:58:17 -0600

    adding new practice

---
+++ dart_tutorial_1/lib/dart_40.dart
@@ -0,0 +1,11 @@
+void main() {
+  // In this we have defined the value of variable b.
+  var b = "GeeksforGeeks";
+  String a = b ?? 'Hello';
+  print(a);
+
+  // In this we have not defined the value of variable c.
+  var c;
+  String d = c ?? 'hello';
+  print(d);
+}
